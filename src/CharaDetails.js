b900dd420420346224bd51cb2d719936046ebfcf
LordChibi <lwatson587@gmail.com>
2023-08-04 18:42:00 -0700

    08/04/23

--- src/CharaDetails.js
+++ src/CharaDetails.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useLocation, useSearchParams } from "react-router-dom";
 
-const BASE_URL="https://www.moogleapi.com/api/v1/characters/search?name=lightning"
-export default class JobList extends React.Component{
-    state={
-        character:{},
-        img:""
-    }
-    componentDidMount(){
+let BASE_URL="https://www.moogleapi.com/api/v1/characters/search?name="
+export default function JobList (){
+    const [character, setCharacter] = useState({})
+    const [img, setImg] = useState("")
+    const location=useLocation()
+    useEffect(()=>{
+        BASE_URL=BASE_URL+(location.pathname.slice(15))
+
         axios.get(BASE_URL).then((res) =>{
             const character=res.data;
-            console.log(character[0].pictures[0].url)
-            this.setState({character: character[0], img: character[0].pictures[0].url})
-        });
-    }
-    render(){
-        
+            
+            setCharacter(character[0])
+            setImg(character[0].pictures[0].url)
+        })
+       
+    },[location]);
+    
         return(
             <div className="Card">
-                <p className="charaName"> Name: {this.state.character.name}</p>
-                <img className="charaImage" src={this.state.img}/>
-                <h2 className="charaAge"> Age: {this.state.character.age}</h2>
-                <h3 className="charaJob"> Job: {this.state.character.job}</h3>
-                <h4 className="charaGame"> Game: {this.state.character.origin}</h4>
-                <p className="charaBio">Bio: {this.state.character.description}</p>
+                <p className="charaName"> Name: {character.name}</p>
+                <img className="charaImage" src={img}/>
+                <h2 className="charaAge"> Age: {character.age}</h2>
+                <h3 className="charaJob"> Job: {character.job}</h3>
+                <h4 className="charaGame"> Game: {character.origin}</h4>
+                <p className="charaBio">Bio: {character.description}</p>
             </div>
             
         )
-    }
 }
